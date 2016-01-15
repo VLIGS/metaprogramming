5407fd9e202a851e6caa505dce5947a9d7259efb
VLIGS <liudmila.veshneva@gmail.com>
2016-01-15 16:57:26 +0000

    examples of set/get Property and methodMissing with comments

--- src/SpringOneExpandoSpecification.groovy
+++ src/SpringOneExpandoSpecification.groovy
@@ -11,7 +11,7 @@ class SpringOneExpandoTest {
     @Test
     public void testMethodAccess(){
         def ex = new SpringOneExpando()
-        ex.multiplyNumbers = { x, y ->
+        ex.multiplyNumbers = { x, y -> //this will call setProperty method and add this closure to the map
             x*y         //here we are assigning value to closure, effectively we are adding a method to this object
         }
         assertEquals(36, ex.multiplyNumbers(4,9))
