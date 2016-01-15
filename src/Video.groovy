6c5e039be44873bbed2be59f756d3307d4f7be16
VLIGS <liudmila.veshneva@gmail.com>
2016-01-15 15:20:29 +0000

    first commit

---
+++ src/Video.groovy
@@ -0,0 +1,7 @@
+ex = new Expando()
+ex.homeTown = 'LND' //this is not standard practice for groove, but specific feature of Expando class
+                    //for Expando class and not just for any class, can just make up a property and assign a value to it
+                    //here we are assigning a value to property that doesn't exist
+
+println "Home town: ${ex.homeTown}"
+
